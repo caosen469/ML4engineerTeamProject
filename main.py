1a8029aee9824cf904b10b6242d913a3703f9ac4
Sihan Cao <sihancao496@gmail.com>
2020-11-03 05:24:09 -0500

    finish y part

--- main.py
+++ main.py
@@ -18,48 +18,52 @@ for file in files:
         
 files = files2.copy()
 
+#%%
+y = np.empty((0,100))
+
 #%% 进入一个文件夹下
-# for i in range()
-i = 0
-subfolder = files[i]
-subpath = path + '\\' + subfolder
-
-#%% 在进入下一级文件夹
-
-subfiles = os.listdir(subpath)
-subfiles2 = []
-for file in subfiles:
-    if file[0:4] == 'mass':
-        subfiles2.append(file)
-
-subfiles = subfiles2.copy()
+for i in range(len(files)):
+   
+    subfolder = files[i]
+    subpath = path + '\\' + subfolder
+    
+    #%% 在进入下一级文件夹
+    
+    subfiles = os.listdir(subpath)
+    subfiles2 = []
+    for file in subfiles:
+        if file[0:4] == 'mass':
+            subfiles2.append(file)
+    
+    subfiles = subfiles2.copy()
+    
+    #%%
+    # 真的进入了
+    for j in range(len(subfiles)):
+        subsubfolder = subfiles[j]
+        subsubpath = subpath +'\\' + subsubfolder
+        
+        # 阅读到profile数据
+        data_files = os.listdir(subsubpath)[0]
+        
+        #打开profiles数据
+        with open(subsubpath + "\\" + data_files) as f:
+            # read_data = f.read()
+            one_file_data = []
+            count = 1 
+            for lines in f:
+                if count >= 5:
+                    one_row = lines.split()
+                    one_file_data.append(float(one_row[3]))
+                count = count + 1
+        
+        one_file_data = np.array(one_file_data).reshape((-1,100))
+                    
+        y = np.append(y, one_file_data, axis=0)
 
 #%%
-# 真的进入了
-# for j in range()
-j=0
-subsubfolder = subfiles[j]
-subsubpath = subpath +'\\' + subsubfolder
-
-# 阅读到profile数据
-data_files = os.listdir(subsubpath)[0]
-
-#打开profiles数据
-with open(subsubpath + "\\" + data_files) as f:
-    # read_data = f.read()
-    one_row_data = []
-    count = 1 
-    for lines in f:
-        if count >= 5:
-            one_row = lines.split()
-            one_row_data.append(float(one_row[3]))
-            # print(one_row[3])
-            print('-------------')
-            print()
-        count = count+1
-    
 # 变成迭代器
-
+np.save('y_data', y)
 #遍历行
 
 
